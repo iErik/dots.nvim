88b4f2a80862d500b5e15130be670a7057a2fc80
Erik Isidore <ericssonico@protonmail.ch>
2026-01-11 22:21:45 -0300

    Vue LSP experiments #0

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,38 +1,5 @@
 local servers = require 'plugins.lsp.servers'
 
-vim.lsp.enable('lua_ls')
-vim.lsp.config('lua_ls', {
-  on_init = function(client)
-    local path = client.workspace_folders[1].name
-
-    if vim.loop.fs_stat(path..'/.luarc.json') or
-       vim.loop.fs_stat(path..'/.luarc.jsonc') then
-      return
-    end
-
-    client.config.settings.Lua = vim.tbl_deep_extend(
-      'force', client.config.settings.Lua, {
-        runtime = { version = 'LuaJIT' },
-
-        -- Make the server aware of Neovim
-        -- runtime files
-        workspace = {
-          checkThirdParty = true,
-          library = {
-            vim.env.VIMRUNTIME
-            -- Depending on the usage, you might want
-            -- to add additional paths here.
-            -- "${3rd}/luv/library"
-            -- "${3rd}/busted/library",
-          }
-        }
-    })
-  end,
-  settings = {
-    Lua = {}
-  }
-})
-
 local mason = {
   -- The directory in which to install packages
   --install_root_dir = path.concat {
@@ -70,10 +37,95 @@ return {
     lazy = false,
     priority = 10,
     config = function ()
+      --local vue_language_server_path = vim.fn.expand '$MASON/packages' .. '/vue-language-server' .. '/node_modules/@vue/language-server'
+
+      local vue_language_server_path = vim.fn.stdpath('data') .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+
       vim.lsp.enable('slint_lsp')
       vim.lsp.enable('ts_ls')
-      --require('lspconfig').slint_lsp.setup({})
-      --require('lspconfig').ts_ls.setup({})
+      vim.lsp.config['ts_ls'] = {
+        filetypes = {
+          'typescript',
+          'javascript',
+          'javascriptreact',
+          'typescriptreact',
+          'vue'
+        },
+        init_options = {
+          plugins = {
+            {
+              name = '@vue/typescript-plugin',
+              location = vue_language_server_path,
+              languages = { 'vue' },
+              configNamespace = 'typescript'
+            }
+          }
+        }
+      }
+
+      vim.lsp.enable('vtsls')
+      vim.lsp.config('vtsls', {
+        settings = {
+          vtsls = {
+            tsserver = {
+              globalPlugins = {
+                {
+                  name = '@vue/typescript-plugin',
+                  location = vue_language_server_path,
+                  languages = { 'vue' },
+                  configNamespace = 'typescript'
+                }
+              },
+            },
+          },
+        },
+        filetypes = {
+          'typescript',
+          'typescriptreact',
+          'javascript',
+          'javascriptreact',
+          'vue'
+        },
+      })
+
+      vim.lsp.enable('vue_ls')
+      vim.lsp.config('vue_ls', {
+        on_init = function(client)
+          client.handlers['tsserver/request'] = function(_, result, context)
+            local ts_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = 'ts_ls' })
+            local vtsls_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = 'vtsls' })
+            local clients = {}
+
+            vim.list_extend(clients, ts_clients)
+            vim.list_extend(clients, vtsls_clients)
+
+            if #clients == 0 then
+              vim.notify('Could not find `vtsls` or `ts_ls` lsp client, `vue_ls` would not work without it.', vim.log.levels.ERROR)
+              return
+            end
+            local ts_client = clients[1]
+
+            local param = unpack(result)
+            local id, command, payload = unpack(param)
+            ts_client:exec_cmd({
+              title = 'vue_request_forward', -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+              command = 'typescript.tsserverRequest',
+              arguments = {
+                command,
+                payload,
+              },
+            }, { bufnr = context.bufnr }, function(_, r)
+                local response = r and r.body
+                -- TODO: handle error or response nil here, e.g. logging
+                -- NOTE: Do NOT return if there's an error or no response, just return nil back to the vue_ls to prevent memory leak
+                local response_data = { { id, response } }
+
+                ---@diagnostic disable-next-line: param-type-mismatch
+                client:notify('tsserver/response', response_data)
+              end)
+          end
+        end,
+      })
 
       vim.lsp.enable('rust_analyzer')
       vim.lsp.config['rust_analyzer'] = {
@@ -134,6 +186,16 @@ return {
       }
     end
   },
+
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "neovim/nvim-lspconfig"
+    },
+    opts = {},
+  },
+
 
   { "hrsh7th/cmp-nvim-lsp" },
   { "hrsh7th/nvim-cmp" },
