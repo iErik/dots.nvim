2f9fef7c9fa328ec17e4d8caeb685385f78da5ec
Erik Isidore <ericssonico@protonmail.ch>
2026-03-22 22:58:57 -0300

    Telescope shim

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -16,6 +16,10 @@ return {
     tag = "0.1.6",
     dependencies = { { "nvim-lua/plenary.nvim" } },
     config = function ()
+      -- ft_to_lang was removed in Neovim 0.10; get_lang is the replacement
+      if not vim.treesitter.language.ft_to_lang then
+        vim.treesitter.language.ft_to_lang = vim.treesitter.language.get_lang
+      end
       require('telescope').setup(config)
     end
   }
